4b5a721db22f98578ed093d96b953319c0a13271
Anders Elfgren <srekel@gmail.com>
2025-04-09 19:06:06 +0200

    Added IRay to zig lib.

--- src/root.zig
+++ src/root.zig
@@ -1,1 +1,2 @@
 pub const IGraphics = @import("../external/The-Forge/Common_3/Interfaces/IGraphics/IGraphics.zig");
+pub const IRay = @import("../external/The-Forge/Common_3/Interfaces/IGraphics/IRay.zig");
